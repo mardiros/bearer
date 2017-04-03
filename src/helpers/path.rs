b2843de93d83f537afa1ad1870e006f74d2e9b64
Guillaume Gauvrit <guillaume@gauvr.it>
2017-04-03 22:22:37 +0200

    Add the list clients helper

--- src/helpers/path.rs
+++ src/helpers/path.rs
@@ -1,12 +1,12 @@
 use std::fs;
 use std::path::Path;
+use std::vec::Vec;
 use tilde_expand::tilde_expand;
 
 use super::super::results::{BearerResult, BearerError};
 
 
-pub fn build_path(config_dir: &str, client_name: &str) -> BearerResult<(String, bool)> {
-
+fn expand_path(config_dir: &str) -> BearerResult<String> {
     let config_dir_expanded = tilde_expand(config_dir.as_bytes());
     let config_dir_expanded = String::from_utf8(config_dir_expanded);
     if let Err(err) = config_dir_expanded {
@@ -15,7 +15,13 @@ pub fn build_path(config_dir: &str, client_name: &str) -> BearerResult<(String, 
                     config_dir,
                     err)));
     }
-    let config_dir_expanded = config_dir_expanded.unwrap();
+    Ok(config_dir_expanded.unwrap())
+}
+
+
+pub fn build_path(config_dir: &str, client_name: &str) -> BearerResult<(String, bool)> {
+
+    let config_dir_expanded = expand_path(config_dir)?;
     let path = config_dir_expanded.clone();
     let path = Path::new(path.as_str());
     if !path.exists() {
@@ -46,6 +52,29 @@ pub fn build_path(config_dir: &str, client_name: &str) -> BearerResult<(String, 
         }
     }
 }
+
+
+pub fn list_clients(config_dir: &str) -> BearerResult<Vec<String>> {
+    let config_dir_expanded = expand_path(config_dir)?;
+    let path = config_dir_expanded.clone();
+    let path = Path::new(path.as_str());
+    if !path.is_dir() {
+        return Err(BearerError::ValueError(
+            format!("Path {} is not a directory",
+                    config_dir_expanded)));
+    }
+    let paths = fs::read_dir(path).unwrap();
+    let mut ret = paths.map(
+            |pth| pth.unwrap().path().file_name().unwrap().to_str().unwrap().to_string()
+        ).filter(
+            |pth| pth.ends_with(".toml")
+        ).map(
+            |pth| pth.as_str()[..pth.len() - 5].to_string()
+        ).collect::<Vec<String>>();
+    ret.sort();
+    Ok(ret)
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -97,4 +126,23 @@ mod tests {
         fs::remove_dir_all(tmpdir).unwrap();
     }
 
+    #[test]
+    fn test_list_clients_ok() {
+        let clients = list_clients("src/tests/conf").unwrap();
+        assert_eq!(
+            clients.as_slice(),
+            &["dummy", "dummy_with_tokens", "invalid"]
+        )
+    }
+
+    #[test]
+    fn test_list_clients_err() {
+        let err = list_clients("not/an/existings/directory");
+        assert_eq!(err.is_err(), true);
+        let err = err.unwrap_err();
+        assert_eq!(
+            err,
+            BearerError::ValueError("".to_string())
+        )
+    }
 }
